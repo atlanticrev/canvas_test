593f1c6656d447d8d5615c4df853902002fa15c2
Atlanticrev <atlanticrev@gmail.com>
2019-08-30 11:03:21 +0300

    Настройки package.json

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+let
+  canvas = document.querySelector('canvas'),
+  width = canvas.width,
+  height = canvas.height,
+  ctx = canvas.getContext('2d');
 
 function Drawer (context) {
   this.ctx = context;
@@ -22,7 +25,17 @@ Drawer.prototype.line = function (x1, y1, x2, y2) {
 };
 
 Drawer.prototype.arc = function (x, y, r, start, end, direction) {
+  this.ctx.beginPath();
   this.ctx.arc(x, y, r, start, end, direction);
+  this.ctx.strokeStyle = 'blue';
+  this.ctx.stroke();
+
+  return this;
+};
+
+Drawer.prototype.circle = function (x, y, r) {
+  this.ctx.beginPath();
+  this.ctx.arc(x, y, r, 0, Math.PI * 2, true);
   this.ctx.strokeStyle = 'blue';
   this.ctx.stroke();
 
@@ -31,7 +44,6 @@ Drawer.prototype.arc = function (x, y, r, start, end, direction) {
 
 let drawer = new Drawer(ctx);
 
-drawer
-  .arc(250, 250, 150, 0, Math.PI * 3 / 2, true)
-  .line(250, 250, 20, 20)
-  .line(250, 250, 240, 40);
+for (let i = 0; i < 10; i++) {
+  drawer.circle(random(0, width), random(0, height), random(10, 20));
+}
